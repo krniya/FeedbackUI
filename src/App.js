71ee30f76c94474f8489a0a6c539f4e86a82e987
Nitish Kumar <krnitish@live.in>
2022-02-08 02:21:49 +0530

    Header Added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Fragment } from "react/cjs/react.production.min";
+import Header from "./components/Header";
+function App() {
+    return (
+        <Fragment>
+            <Header>
+                <div className="container">
+                    <h1>My App</h1>
+                </div>
+            </Header>
+        </Fragment>
+    );
+}
+
+export default App;
