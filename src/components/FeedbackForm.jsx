227720fcb4656afb96bed0c3c8dc2c3dcf1d76e2
Nitish Kumar <krnitish@live.in>
2022-02-12 02:20:57 +0530

    Feedback limitaion and validation added

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,7 +4,19 @@ import Button from "../shared/Button";
 
 function FeedbackForm() {
     const [text, setText] = useState("");
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [message, setMessage] = useState("");
     const handleTextChange = (e) => {
+        if (text === "") {
+            setBtnDisabled(true);
+            setMessage("");
+        } else if (text !== "" && text.trim().length <= 10) {
+            setBtnDisabled(true);
+            setMessage("Text must be at least 10 characters");
+        } else {
+            setMessage(null);
+            setBtnDisabled(false);
+        }
         setText(e.target.value);
     };
     return (
@@ -18,8 +30,11 @@ function FeedbackForm() {
                         value={text}
                         onChange={handleTextChange}
                     />
-                    <Button type="submit">Send</Button>
+                    <Button type="submit" isDisabled={btnDisabled}>
+                        Send
+                    </Button>
                 </div>
+                {message && <div className="message">{message}</div>}
             </form>
         </Card>
     );
